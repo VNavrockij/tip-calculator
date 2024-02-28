28c51a4baeef3be21b9794e5fb6ace8b828235c0
VNavrockij <vitalik.nav@gmail.com>
2024-02-28 18:26:56 +0200

    add: some properties to BillInputView

--- tip-calculator/Views/BillInputView.swift
+++ tip-calculator/Views/BillInputView.swift
@@ -9,6 +9,34 @@ import UIKit
 
 class BillInputView: UIView {
 
+    private lazy var headerView: HeaderView = {
+        return HeaderView()
+    }()
+
+    private lazy var textFieldContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.addCornerRadius(radius: 8.0)
+        return view
+    }()
+
+    private lazy var currencyDenominationLabel: UILabel = {
+        let label = LabelFactory.build(
+            text: "$",
+            font: ThemeFont.bold(ofSize: 24))
+        label.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        return label
+    }()
+
+    private lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .none
+        textField.font = ThemeFont.semibold(ofSize: 28)
+        textField.keyboardType = .decimalPad
+        textField.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        return textField
+    }()
+
     init() {
         super.init(frame: .zero)
         layout()
@@ -22,4 +50,20 @@ class BillInputView: UIView {
         backgroundColor = .green
     }
 
+}
+
+class HeaderView: UIView {
+
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func layout() {
+        backgroundColor = .red
+    }
 }
