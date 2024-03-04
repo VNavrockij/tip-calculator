42fc1d4df6f372ad0cfed344e6bb6e6078139139
VNavrockij <vitalik.nav@gmail.com>
2024-03-04 20:13:32 +0200

    add: properties for HeaderView

--- tip-calculator/Views/BillInputView.swift
+++ tip-calculator/Views/BillInputView.swift
@@ -10,7 +10,11 @@ import UIKit
 class BillInputView: UIView {
 
     private lazy var headerView: HeaderView = {
-        return HeaderView()
+        let view = HeaderView()
+        view.configure(
+            topText: "Enter",
+            bottomText: "your bill")
+        return view
     }()
 
     private lazy var textFieldContainerView: UIView = {
@@ -103,6 +107,34 @@ class BillInputView: UIView {
 
 class HeaderView: UIView {
 
+    private let topLabel: UILabel = {
+        LabelFactory.build(
+            text: nil,
+            font: ThemeFont.bold(ofSize: 18))
+    }()
+
+    private let bottomLabel: UILabel = {
+        LabelFactory.build(
+            text: nil,
+            font: ThemeFont.regular(ofSize: 16))
+    }()
+
+    private let topSpacerView = UIView()
+    private let bottomSpacerView = UIView()
+
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [
+            topSpacerView,
+            topLabel,
+            bottomLabel,
+            bottomSpacerView
+        ])
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.spacing = -4
+        return stackView
+    }()
+
     init() {
         super.init(frame: .zero)
         layout()
@@ -113,6 +145,19 @@ class HeaderView: UIView {
     }
 
     private func layout() {
-        backgroundColor = .red
+        addSubview(stackView)
+
+        stackView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+
+        topSpacerView.snp.makeConstraints { make in
+            make.height.equalTo(bottomSpacerView)
+        }
+    }
+
+    func configure(topText: String, bottomText: String) {
+        topLabel.text = topText
+        bottomLabel.text = bottomText
     }
 }
